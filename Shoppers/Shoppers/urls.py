f92801f09eed513b203a759d7a2480b5d4379c6c
Karthik <karthiksrao987@gmail.com>
2026-04-18 22:06:31 +0530

    url fix

--- Shoppers/Shoppers/urls.py
+++ Shoppers/Shoppers/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('dashboard/',include('dashboard.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
